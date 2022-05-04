5018060385a275985f773120714a172ced22b32e
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-04 22:20:35 +0200

    finiished modularizing slidericons

---
+++ src/components/RightMenu/Waves/Icons/DistanceRight.tsx
@@ -0,0 +1,24 @@
+const DistanceRight: React.FunctionComponent = () => {
+  return (
+    <>
+      <path
+        d="M15 45.8134C199 188.412 202.5 -70.0069 403 45.8134"
+        stroke="white"
+        stroke-width="30"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-dasharray="1 1"
+      />
+      <path
+        d="M41 241.457C225 391.661 228.5 119.459 429 241.457"
+        stroke="white"
+        stroke-width="30"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-dasharray="1 1"
+      />
+    </>
+  );
+};
+
+export default DistanceRight;
--- src/components/RightMenu/Waves/Shape.tsx
+++ src/components/RightMenu/Waves/Shape.tsx
@@ -16,8 +16,11 @@ import React, { Dispatch, SetStateAction } from 'react';
 import SliderIconWrapper from '../SliderIconWrapper';
 import BalanceLeft from './Icons/BalanceLeft';
 import BalanceRight from './Icons/BalanceRight';
+import ComplexityRight from './Icons/ComplexityRight';
+import ComplexityLeft from './Icons/ComplexityLeft';
 import VelocityLeft from './Icons/VelocityLeft';
 import VelocityRight from './Icons/VelocityRight';
+import DistanceLeft from './Icons/DistanceLeft';
 
 export interface IShapeProps {
   balance: number;
@@ -95,7 +98,10 @@ const Shape: React.FunctionComponent<IShapeProps> = ({
           <SliderThumb />
         </Slider>
 
-        <SliderIconWrapper viewBox={"0 0 412 286"} onClick={() => velocity < 250 && setVelocity(velocity + 25)}>
+        <SliderIconWrapper
+          viewBox={'0 0 412 286'}
+          onClick={() => velocity < 250 && setVelocity(velocity + 25)}
+        >
           <VelocityRight />
         </SliderIconWrapper>
       </HStack>
@@ -104,55 +110,22 @@ const Shape: React.FunctionComponent<IShapeProps> = ({
       <Heading as="h4" size="xs" opacity={0.5}>
         Complexity
       </Heading>
+
       <HStack>
-        <Icon
-          onClick={() => breaks > 0 && setBreaks(breaks - 1)}
-          boxSize="9"
-          viewBox="0 0 420 78"
-          rounded="full"
-          p="2"
-          bg="#262e3a"
-          transition="0.3s"
-          _hover={{ background: '#373d48', cursor: 'pointer' }}
-          fill="none"
-        >
-          <path
-            xmlns="http://www.w3.org/2000/svg"
-            d="M16 34.8824C200 124 203.5 -37.5 404 34.8824"
-            stroke="white"
-            stroke-width="30"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-dasharray="1 1"
-          />
-        </Icon>
+        <SliderIconWrapper viewBox={'0 0 420 78'} onClick={() => breaks > 0 && setBreaks(breaks - 1)}>
+          <ComplexityLeft />
+        </SliderIconWrapper>
+
         <Slider aria-label="complexity" value={breaks} min={0} max={20} onChange={(val) => setBreaks(val)}>
           <SliderTrack>
             <SliderFilledTrack />
           </SliderTrack>
           <SliderThumb />
         </Slider>
-        <Icon
-          onClick={() => breaks < 20 && setBreaks(breaks + 1)}
-          boxSize="9"
-          viewBox="0 0 432 158"
-          rounded="full"
-          p="2"
-          bg="#262e3a"
-          transition="0.3s"
-          _hover={{ background: '#373d48', cursor: 'pointer' }}
-          fill="none"
-        >
-          <path
-            xmlns="http://www.w3.org/2000/svg"
-            d="M414 15.5C421.5 43.1667 417 142.5 367 142.5C317 142.5 342.5 50 290 50C237.5 50 252 142.5 202 142.5C157 142.5 170 94 132 94C86.0001 94 107 142.5 61.0001 142.5C28 142.5 16 121 16 121"
-            stroke="white"
-            stroke-width="30"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-dasharray="1 1"
-          />
-        </Icon>
+
+        <SliderIconWrapper viewBox={'0 0 432 158'} onClick={() => breaks < 20 && setBreaks(breaks + 1)}>
+          <ComplexityRight />
+        </SliderIconWrapper>
       </HStack>
 
       {/* ----- LAYERS INPUT ------ */}
@@ -172,34 +145,10 @@ const Shape: React.FunctionComponent<IShapeProps> = ({
         Layer Distance
       </Heading>
       <HStack>
-        <Icon
-          onClick={() => distance > 0 && setDistance(distance - 1)}
-          boxSize="9"
-          viewBox="0 0 446 123"
-          rounded="full"
-          p="2"
-          bg="#262e3a"
-          transition="0.3s"
-          _hover={{ background: '#373d48', cursor: 'pointer' }}
-          fill="none"
-        >
-          <path
-            d="M16 34.257C200 123.375 203.5 -38.1254 404 34.257"
-            stroke="white"
-            stroke-width="30"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-dasharray="1 1"
-          />
-          <path
-            d="M42 90.257C226 179.375 229.5 17.8746 430 90.257"
-            stroke="white"
-            stroke-width="30"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-dasharray="1 1"
-          />
-        </Icon>
+        <SliderIconWrapper viewBox={'0 0 446 123'} onClick={() => distance > 0 && setDistance(distance - 1)}>
+          <DistanceLeft />
+        </SliderIconWrapper>
+
         <Slider
           aria-label="layer-distance"
           value={distance}
@@ -212,34 +161,10 @@ const Shape: React.FunctionComponent<IShapeProps> = ({
           </SliderTrack>
           <SliderThumb />
         </Slider>
-        <Icon
-          onClick={() => distance < 10 && setDistance(distance + 1)}
-          boxSize="9"
-          viewBox="0 0 445 303"
-          rounded="full"
-          p="2"
-          bg="#262e3a"
-          transition="0.3s"
-          _hover={{ background: '#373d48', cursor: 'pointer' }}
-          fill="none"
-        >
-          <path
-            d="M15 45.8134C199 188.412 202.5 -70.0069 403 45.8134"
-            stroke="white"
-            stroke-width="30"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-dasharray="1 1"
-          />
-          <path
-            d="M41 241.457C225 391.661 228.5 119.459 429 241.457"
-            stroke="white"
-            stroke-width="30"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-dasharray="1 1"
-          />
-        </Icon>
+
+        <SliderIconWrapper viewBox={'0 0 445 303'} onClick={() => distance < 10 && setDistance(distance + 1)}>
+          <DistanceLeft />
+        </SliderIconWrapper>
       </HStack>
     </>
   );

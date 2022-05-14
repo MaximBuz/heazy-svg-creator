933b00b49cf5516eae7a689894ee4b62d8275918
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-15 00:18:02 +0200

    initial commit

--- heazy-landing-page/pages/_app.tsx
+++ heazy-landing-page/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import theme from '../utils/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme} resetCSS={true}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp

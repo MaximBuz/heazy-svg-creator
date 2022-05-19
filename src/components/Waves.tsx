dceaa8f392930b593f7f0bc0d96e316b98e42d31
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-19 16:14:20 +0200

    refactored waves

--- src/components/Waves.tsx
+++ src/components/Waves.tsx
@@ -4,40 +4,38 @@ import { peakWavePath } from '../utils/calculations/peakWavePath';
 import { IWaveProps } from '../utils/types/waveProps';
 import SvgCanvas from './SvgCanvas';
 
+const Waves: React.FunctionComponent<IWaveProps> = (props) => {
+  // set up params that are needed to generate a path
+  const pathParams = [
+    props.seed,
+    props.width,
+    props.height,
+    props.balance,
+    props.velocity,
+    props.breaks,
+    props.stacks,
+    props.distance,
+    props.stroke,
+  ] as const;
 
-const Waves: React.FunctionComponent<IWaveProps> = ({
-  svgRef,
-  type,
-  seed,
-  width,
-  height,
-  balance,
-  velocity,
-  breaks,
-  stacks,
-  distance,
-  startWaveColor,
-  stopWaveColor,
-  bgColor,
-  shadowX,
-  shadowY,
-  shadowSD,
-  shadowColor,
-  stroke,
-  strokeWidth,
-  strokeShrink,
-  direction
-}) => {
+  // destructure some params
+  const { width, height, svgRef, type } = props;
+  const { stroke, strokeShrink, strokeWidth } = props;
+  const { shadowColor, shadowSD, shadowX, shadowY } = props;
+  const { bgColor, startWaveColor, stopWaveColor } = props;
+
+  // generate paths
   let wavesData;
-  if (type === 'smooth') {
-    wavesData = smoothWavePath(seed, width, height, balance, velocity, breaks, stacks, distance, stroke);
+  if (props.type === 'smooth') {
+    wavesData = smoothWavePath(...pathParams);
   } else {
-    wavesData = peakWavePath(seed, width, height, balance, velocity, breaks, stacks, distance, stroke);
+    wavesData = peakWavePath(...pathParams);
   }
   const randomClassId = Math.round(Math.random() * 100);
+
   return (
     <SvgCanvas width={width} height={height} ref={svgRef}>
-      <g transform-origin="center" transform={"scale(1, 1) rotate(0)"}>
+      <g transform-origin="center" transform={'scale(1, 1) rotate(0)'}>
         <rect x="0" y="0" width={width} height={height} fill={bgColor}></rect>
         <linearGradient id={`linear-gradient-${type}-${randomClassId}`}>
           <stop offset="0%" stopColor={startWaveColor} stopOpacity="100%" />
@@ -47,12 +45,7 @@ const Waves: React.FunctionComponent<IWaveProps> = ({
         {/* in the shadow you have to put in either x and width or y and height for shadows to stay in box */}
         {!stroke && (
           <filter id={`shadow-${type}-${randomClassId}`} x={0} width="100%" y="-20%" height="150%">
-            <feDropShadow
-              dx={shadowX}
-              dy={shadowY}
-              stdDeviation={shadowSD}
-              floodColor={shadowColor}
-            />
+            <feDropShadow dx={shadowX} dy={shadowY} stdDeviation={shadowSD} floodColor={shadowColor} />
           </filter>
         )}
         {wavesData.map((wave, index) => (

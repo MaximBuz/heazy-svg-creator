46448494c4775aaa09d7bf8ebf3d1fddfba3d657
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-20 22:22:00 +0200

    finished c2a

--- heazy-landing-page/Sections/Call2Action.tsx
+++ heazy-landing-page/Sections/Call2Action.tsx
@@ -1,4 +1,4 @@
-import { Flex, HStack, Divider, Heading, Text, Container } from '@chakra-ui/react';
+import { Flex, HStack, Divider, Heading, Text, Container, Button } from '@chakra-ui/react';
 import React from 'react';
 import OverviewBG from '../public/overview-bg.png';
 import SectionDivider from '../Components/SectionDivider';
@@ -8,22 +8,21 @@ export interface ICall2ActionProps {}
 const Call2Action: React.FunctionComponent<ICall2ActionProps> = (props) => {
   return (
     <Flex
-      id="features"
+      backgroundImage="radial-gradient(circle farthest-corner at -75% 0%,#0F1119 0%,rgb(15, 17, 25,0) 48%),radial-gradient(circle farthest-corner at 25% -80%,#12141c 50%,rgb(15, 17, 25,0)100%),radial-gradient(circle farthest-corner at -55% -100%,rgb(15, 17, 25,0) 55%,rgba(13,17,23,.28) 65%,rgb(15, 17, 25,0) 68%),radial-gradient(circle farthest-corner at -33% -75%,#00c58d 48%,rgba(131,5,49,0) 56%),radial-gradient(circle farthest-side at 0 50%,rgb(15, 17, 25,0) 64%,rgba(1,2,36,.4) 69%,rgb(15, 17, 25,0) 81%),radial-gradient(circle farthest-corner at 0 50%,rgb(15, 17, 25,0) 33%,#05f 51%,rgb(15, 17, 25,0) 72%)"
       position="relative"
       backgroundRepeat="no-repeat"
       backgroundPosition="bottom"
       backgroundSize="contain"
       direction="column"
+      textAlign="center"
+      gap="20px"
+      justifyContent="center"
+      alignItems="center"
       width="100%"
       height="100vh"
-      justifyContent="flex-start"
-      alignItems="center"
-      textAlign="center"
-      gap="50px"
     >
-      <SectionDivider text="Start designing" />
-      <Heading fontWeight="800" fontSize="4em">
-        Get started with<br></br>
+      <Heading fontWeight="800" fontSize="4em" lineHeight={1}>
+        What are you wating for?<br></br>
         <span
           style={{
             backgroundImage: 'linear-gradient(207deg,#00c58d 23%,#05f 87%)',
@@ -34,8 +33,22 @@ const Call2Action: React.FunctionComponent<ICall2ActionProps> = (props) => {
         >
           Heazy
         </span>{' '}
-        today
+        is waiting for you!
       </Heading>
+      <Text fontSize='20px'>
+        No sign ups, logins, credit cards etc. We promise!
+      </Text>
+      <Button
+        as="a"
+        href="https://app.heazy.studio/"
+        rounded="full"
+        size="lg"
+        bg='#05f'
+        color="white"
+        _hover={{transform: 'scale(1.05)' }}
+      >
+        Try it out!
+      </Button>
     </Flex>
   );
 };

70e8f9854ea2b3af37f9bd7f9dbbf804f624c86b
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-21 16:27:29 +0200

    fixed regex bug

--- src/utils/regexHelpers.ts
+++ src/utils/regexHelpers.ts
@@ -1,12 +1,9 @@
 function styleToJsx(html: string): string {
-  // get the full style tag
-  const [styleTag] = html.match(/(style=")(.*)(")/g);
-
   // get only the values within the style tag
-  const styles = styleTag.match(/(")(.*)(")/g)[0].replaceAll('"', '');
+  const styles = html.match(/(")(.*)(")/g)[0].replaceAll('"', '');
 
-  // clean styles from semi-colons and transitions
-  const cleanedStyles = styles.replace(/transition(.*)(; )/gi, '').replaceAll(';', '');
+  // clean styles from semi-colons
+  const cleanedStyles = styles.replaceAll(';', '');
 
   // convert style to a valid jsx style object
   return (
@@ -19,9 +16,31 @@ function styleToJsx(html: string): string {
   );
 }
 
+function stringify(obj) {
+  const cleaned = JSON.stringify(obj, null, 2);
+
+  return cleaned.replace(/^[\t ]*"[^:\n\r]+(?<!\\)":/gm, function (match) {
+    return match.replace(/"/g, '');
+  });
+}
+
+function cssToJsx(string) {
+  string = string.replace('style="', '').slice(0, -1);
+  string = string.replaceAll('&quot;', "'");
+  string = `{"${string.replace(/; /g, '", "').replace(/: /g, '": "').replace(';', '')}"}`;
+
+  const obj = JSON.parse(string);
+
+  const keyValues = Object.keys(obj).map((key) => {
+    var camelCased = key.replace(/-[a-z]/g, (g) => g[1].toUpperCase());
+    return { [camelCased]: obj[key] };
+  });
+  return 'style={' + stringify(Object.assign({}, ...keyValues)) + '}';
+}
+
 export function svgToJsx(svg: string): string {
   return svg
-    .replaceAll(/(style=")(.*)(")/g, styleToJsx(svg))
+    .replaceAll(/(?:style=")(.*?)\"/g, (match) => cssToJsx(match))
     .replaceAll('stroke-width', 'strokeWidth')
     .replaceAll('stroke-linecap', 'strokeLinecap')
     .replaceAll('stroke-linejoin', 'strokeLinejoin')

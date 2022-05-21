ea3a8f5ecc1d85c29f879803a9a80a44420ea8af
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-21 16:28:07 +0200

    removed unused function

--- src/utils/regexHelpers.ts
+++ src/utils/regexHelpers.ts
@@ -1,21 +1,3 @@
-function styleToJsx(html: string): string {
-  // get only the values within the style tag
-  const styles = html.match(/(")(.*)(")/g)[0].replaceAll('"', '');
-
-  // clean styles from semi-colons
-  const cleanedStyles = styles.replaceAll(';', '');
-
-  // convert style to a valid jsx style object
-  return (
-    'style={{ ' +
-    cleanedStyles.replace(
-      /([\w.-]+):\s*([^\s;]*)/g,
-      (x, y, z) => `${y.replace(/-(.)/g, (m, n) => n.toUpperCase())}: '${z}'`
-    ) +
-    '}}'
-  );
-}
-
 function stringify(obj) {
   const cleaned = JSON.stringify(obj, null, 2);
 

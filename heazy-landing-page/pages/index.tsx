334e3780a1c94dd159af7fdb0f2895af4e7fa102
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-23 16:12:57 +0200

    added seo meta tags

--- heazy-landing-page/pages/index.tsx
+++ heazy-landing-page/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Container } from '@chakra-ui/react';
+import Meta from '../partials/seo-meta';
+import { Box } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Call2Action from '../Sections/Call2Action';
@@ -9,37 +10,45 @@ import Inspirations from '../Sections/Inspirations';
 
 const Home: NextPage = () => {
   return (
-    <div style={{ fontFamily: 'Karla, sans-serif', color: '#c5cfd9' }}>
-      <Head>
-        <title>Heazy - SVG Studio</title>
-        <meta name="description" content="Generate unique SVG design assets with ease" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Meta
+        title="Generate unique SVG design assets with ease | Heazy"
+        desc="An intuitive design studio in your browser for creating and exporting unique SVG desings as PNG, SVG or React JavaScript and TypeScript snippets."
+        canonical="https://heazy.studio"
+        css="/static/css/styles.css"
+        js="/static/js/scripts.js"
+      />
+      <div style={{ fontFamily: 'Karla, sans-serif', color: '#c5cfd9' }}>
+        <Head>
+          <title>Heazy - SVG Studio</title>
+          <meta name="description" content="Generate unique SVG design assets with ease" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <Box
-          width="100%"
-          className="gradient-bg"
-          padding={{base: "0 1.5em",lg:"0 10em"}}
-          bgPos="top"
-          bgImage="linear-gradient(0,rgb(18, 20, 28,0) 7%,rgb(18, 20, 28, 0) 20%),radial-gradient(circle farthest-side at -170% 170%,#1A202C 48%,rgb(18, 20, 28,0) 65%),radial-gradient(circle farthest-corner at -55% -125%,rgb(18, 20, 28,0) 50%,rgb(18, 20, 28) 70%,rgb(18, 20, 28,0) 72%),radial-gradient(circle farthest-corner at 0 -50%,rgb(18, 20, 28) 32%,hsla(0,0%,100%,0) 62%),radial-gradient(circle farthest-side at 0 -25%,#007252 50%,rgb(18, 20, 28,0) 72%),radial-gradient(circle farthest-corner at 50% -100%,#05f 26%,rgb(18, 20, 28,0) 72%);"
-        >
-          {/*-------- HERO SECTION --------*/}
-          <Hero />
-        </Box>
+        <main>
+          <Box
+            width="100%"
+            className="gradient-bg"
+            padding={{ base: '0 1.5em', lg: '0 10em' }}
+            bgPos="top"
+            bgImage="linear-gradient(0,rgb(18, 20, 28,0) 7%,rgb(18, 20, 28, 0) 20%),radial-gradient(circle farthest-side at -170% 170%,#1A202C 48%,rgb(18, 20, 28,0) 65%),radial-gradient(circle farthest-corner at -55% -125%,rgb(18, 20, 28,0) 50%,rgb(18, 20, 28) 70%,rgb(18, 20, 28,0) 72%),radial-gradient(circle farthest-corner at 0 -50%,rgb(18, 20, 28) 32%,hsla(0,0%,100%,0) 62%),radial-gradient(circle farthest-side at 0 -25%,#007252 50%,rgb(18, 20, 28,0) 72%),radial-gradient(circle farthest-corner at 50% -100%,#05f 26%,rgb(18, 20, 28,0) 72%);"
+          >
+            {/*-------- HERO SECTION --------*/}
+            <Hero />
+          </Box>
 
-        {/*-------- FEATURES SECTION --------*/}
-        <Features />
+          {/*-------- FEATURES SECTION --------*/}
+          <Features />
 
-        {/*-------- INSPIRATIONS SECTION --------*/}
-        <Inspirations />
+          {/*-------- INSPIRATIONS SECTION --------*/}
+          <Inspirations />
 
-        {/*-------- C2A SECTION --------*/}
-        <Call2Action/>
-
-      </main>
-      <footer></footer>
-    </div>
+          {/*-------- C2A SECTION --------*/}
+          <Call2Action />
+        </main>
+        <footer></footer>
+      </div>
+    </>
   );
 };
 
---
+++ heazy-landing-page/partials/seo-meta.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+const Meta = (props: { title: string; desc: string; canonical: string; css: string; js: string }) => (
+  <Head>
+    <title></title>
+    <meta name="description" content={props.desc} />
+    <meta property="og:type" content="website" />
+    <meta name="og:title" property="og:title" content={props.title} />
+    <meta name="og:description" property="og:description" content="" />
+    <meta property="og:site_name" content={props.desc} />
+    <meta property="og:url" content={props.canonical} />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content={props.title} />
+    <meta name="twitter:description" content={props.desc} />
+    <meta name="twitter:url" content={props.canonical} />
+    <link rel="icon" type="image/png" href="/static/images/favicon.ico" />
+    <link rel="apple-touch-icon" href="/static/images/favicon.ico" />
+    <meta property="og:image" content="" />
+    <meta name="twitter:image" content="" />
+    <link rel="canonical" href={props.canonical} />
+  </Head>
+);
+export default Meta;

291c6b122612c4034daee7b7843b7f183b79bb07
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-29 11:59:27 +0200

    added schema export

--- server/graphql/schema/index.ts
+++ server/graphql/schema/index.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 // Type Definitions
 import { Bubble } from './Bubble';
 import { Corners } from './Corners';
@@ -7,18 +9,20 @@ import { Query } from './Query';
 import { User } from './User';
 import { Waves } from './Waves';
 
-export const typeDefs = `
-  ${User}
+export const typeDefs =
+`${User}
 
-  ${Bubble}
+${Bubble}
 
-  ${Waves}
+${Waves}
 
-  ${Corners}
+${Corners}
 
-  ${Marker}
+${Marker}
 
-  ${Query}
+${Query}
 
-  ${Mutation}
-`;
+${Mutation}`;
+
+// saves a graphql schema on runtime
+// fs.writeFileSync('./graphql/schema.graphql', typeDefs,);

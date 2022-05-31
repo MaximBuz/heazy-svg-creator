4ea273630be6400994223d6f54750abd123b4ebb
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-31 09:37:43 +0200

    added view counter to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./App.css"
+import './App.css';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider, ColorModeScript} from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import theme from './utils/theme';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <ChakraProvider resetCSS={true} theme={theme}>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.goatcounter = {
+          path: function(p) { return "app." + p }
+      }`,
+        }}
+      />
+      <script
+        data-goatcounter="https://heazy.goatcounter.com/count"
+        async
+        src="//gc.zgo.at/count.js"
+      ></script>
+
       <App />
     </ChakraProvider>
   </React.StrictMode>

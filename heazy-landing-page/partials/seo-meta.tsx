d8fceede2b0ab80dddbf214c25525ae4d247f6c7
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-31 09:22:13 +0200

    changed script tag to top-most component

--- heazy-landing-page/partials/seo-meta.tsx
+++ heazy-landing-page/partials/seo-meta.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head';
-import Script from 'next/script';
 const Meta = (props: { title: string; desc: string; canonical: string; css: string; js: string }) => (
   <Head>
-    <Script data-goatcounter="https://heazy.goatcounter.com/count" async src="//gc.zgo.at/count.js"></Script>
     <title></title>
     <meta name="description" content={props.desc} />
     <meta property="og:type" content="website" />

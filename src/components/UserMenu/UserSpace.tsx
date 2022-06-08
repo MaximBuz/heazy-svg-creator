735edca2da083b1bad12b6e13bd42fb33c07eea5
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-08 15:12:42 +0200

    created new seed with actual firebase user

--- src/components/UserMenu/UserSpace.tsx
+++ src/components/UserMenu/UserSpace.tsx
@@ -1,14 +1,14 @@
 import { RaceBy } from '@uiball/loaders';
 import React from 'react';
 import { useAuth } from '../../contexts/Auth';
-import { useUserByFirebaseIdQuery } from '../../graphql/generated';
+import { useGetUserByFirebaseIdQuery } from '../../graphql/generated';
 import { endpoint, headers } from '../../utils/apiConfig';
 
 export interface IUserSpaceProps {}
 
 const UserSpace: React.FunctionComponent<IUserSpaceProps> = (props) => {
   const { currentUser } = useAuth();
-  const userQuery = useUserByFirebaseIdQuery({ endpoint, fetchParams: { headers } }, { id: currentUser.uid });
+  const userQuery = useGetUserByFirebaseIdQuery({ endpoint, fetchParams: { headers } }, { id: currentUser.uid });
 
   if (userQuery.isLoading) {
     return <RaceBy size={150} lineWeight={5} speed={1.4} color="white" />;

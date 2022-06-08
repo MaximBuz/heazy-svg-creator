3ca13ca41f34ee5ed26c8fe6a3f73c6eec6831ee
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-08 16:38:37 +0200

    added error picture to userspace

--- src/features/UserMenu/UserSpace.tsx
+++ src/features/UserMenu/UserSpace.tsx
@@ -1,22 +1,43 @@
-import { RaceBy } from '@uiball/loaders';
-import React from 'react';
+import { Box, Flex, Heading, Image } from '@chakra-ui/react';
+import { Jelly } from '@uiball/loaders';
+import React, { memo } from 'react';
 import { useAuth } from '../../contexts/Auth';
 import { useGetUserByFirebaseIdQuery } from '../../graphql/generated';
 import { endpoint, headers } from '../../utils/apiConfig';
+import ErrorImg from '../../assets/Error.svg';
 
 export interface IUserSpaceProps {}
 
-const UserSpace: React.FunctionComponent<IUserSpaceProps> = (props) => {
+const UserSpace: React.FunctionComponent<IUserSpaceProps> = memo((props) => {
   const { currentUser } = useAuth();
-  const userQuery = useGetUserByFirebaseIdQuery({ endpoint, fetchParams: { headers } }, { id: currentUser.uid });
+  const userQuery = useGetUserByFirebaseIdQuery(
+    { endpoint, fetchParams: { headers } },
+    { id: currentUser.uid }
+  );
 
   if (userQuery.isLoading) {
-    return <RaceBy size={150} lineWeight={5} speed={1.4} color="white" />;
+    return (
+      <Flex width="100%" height="100%" justifyContent="center" alignItems="center">
+        <Jelly size={80} speed={1} color="#363E4A" />
+      </Flex>
+    );
   }
   if (userQuery.isError) {
-    return <>Error fetching user</>;
+  return (
+    <Flex direction="column" mt="1em" textAlign="center" gap="10px" height="90%" justifyContent="center">
+      <Image pos="relative" right="15px" src={ErrorImg} h="20%"></Image>
+      <Box mt="1em">
+        <Heading as="h4" size="md" fontWeight={800}>
+          oooops...{' '}
+        </Heading>
+        <Heading as="h5" size="md" fontWeight={300}>
+          Something went wrong!
+        </Heading>
+      </Box>
+    </Flex>
+  );
   }
-  return <p>{userQuery.isSuccess && userQuery.data?.user?.userName}</p>;
-};
+  return <></>;
+});
 
 export default UserSpace;

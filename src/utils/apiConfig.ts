e192d9bd2579d0696e0ce41a9840b65a850ac73b
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-13 20:58:42 +0200

    changed endpoint

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,4 +1,4 @@
-export const endpoint = 'http://localhost:4000/graphql';
+export const endpoint = 'https://heazy-backend.herokuapp.com/graphql';
 export const headers = (idToken: string) => {
   if (idToken) return { 'Content-Type': 'application/json', Authorization: 'Bearer ' + idToken };
   return { 'Content-Type': 'application/json' };

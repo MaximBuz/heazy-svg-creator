93b14470a582d693e6bd5349448b0ba90e2b5433
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-13 21:40:59 +0200

    firebase

--- server/firebase.ts
+++ server/firebase.ts
@@ -2,10 +2,10 @@ import { applicationDefault, initializeApp } from 'firebase-admin/app';
 var firebaseCert = require('firebase-admin');
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(JSON.parse(process.env.GOOGLE_CREDS as string));
-
+// @ts-ignore
+console.log(JSON.parse(process.env.GOOGLE_CREDS));
 const admin = initializeApp({
+  // @ts-ignore
   credential: firebaseCert.credential.cert(JSON.parse(process.env.GOOGLE_CREDS)),
 });
-
 export default admin;

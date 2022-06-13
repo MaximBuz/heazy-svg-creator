7a780ab353c99a0166a6f554cc5dd7d60c14c2eb
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-13 21:38:23 +0200

    added log

--- server/firebase.ts
+++ server/firebase.ts
@@ -2,6 +2,7 @@ import { applicationDefault, initializeApp } from 'firebase-admin/app';
 var firebaseCert = require('firebase-admin');
 const dotenv = require('dotenv');
 dotenv.config();
+console.log(JSON.parse(process.env.GOOGLE_CREDS as string));
 
 const admin = initializeApp({
   credential: firebaseCert.credential.cert(JSON.parse(process.env.GOOGLE_CREDS)),

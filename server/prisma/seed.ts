604b2d10c2bce6efc8472d80f8f394fa33d29055
Maxim Buz <85324130+MaximBuz@users.noreply.github.com>
2023-07-07 15:10:59 +0000

    chore: remove unnecessary code and comments

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -2,23 +2,14 @@ import { PrismaClient, Prisma } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const userData: Prisma.UserCreateInput = {
-  firebaseId: 'tlSixZcFlJaGB8P2HqmScEuXF6o1',
-  email: 'mbuz.maxim@gmail.com',
-  userName: 'codenameVandal',
-};
 
 async function main() {
-  // await prisma.designType.createMany({
-  //   data: [{ name: 'waves' }, { name: 'bubble' }, { name: 'corners' }, { name: 'marker' }, { name: 'isolines' }],
-  // });
   await prisma.designType.create({data: {name: "waves"}})
   await prisma.designType.create({data: {name: "bubble"}})
   await prisma.designType.create({data: {name: "corners"}})
   await prisma.designType.create({data: {name: "marker"}})
   await prisma.designType.create({data: {name: "isolines"}})
   await prisma.designType.create({data: {name: "flare"}})
-  await prisma.user.create({ data: userData });
 }
 
 main()

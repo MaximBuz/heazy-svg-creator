6c7d055da4a8f735d5a9996ac890c0f8ac42ad6d
Maxim Buz <mbuz.maxim@gmail.com>
2023-07-10 14:22:41 +0200

    fix: remove unused filter button

--- src/features/UserMenu/UserSpace.tsx
+++ src/features/UserMenu/UserSpace.tsx
@@ -1,23 +1,32 @@
-import React, { memo, useState } from 'react';
-import { useAuth } from '../../contexts/Auth';
-import { Design, useGetUserByFirebaseIdQuery } from '../../graphql/generated';
-import { endpoint, headers } from '../../utils/apiConfig';
+import React, { memo, useState } from "react";
+import { useAuth } from "../../contexts/Auth";
+import { Design, useGetUserByFirebaseIdQuery } from "../../graphql/generated";
+import { endpoint, headers } from "../../utils/apiConfig";
 
 // Design
-import { Box, Flex, Heading, HStack, Icon, Text } from '@chakra-ui/react';
-import { Accordion, AccordionItem, AccordionButton, AccordionPanel, Input } from '@chakra-ui/react';
-import Templates from './Templates';
-import Explore from './Explore';
-import QueryError from '../../components/queryError';
-import QueryLoading from '../../components/queryLoading';
+import { Box, Flex, Heading, HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  Input,
+} from "@chakra-ui/react";
+import Templates from "./Templates";
+import Explore from "./Explore";
+import QueryError from "../../components/queryError";
+import QueryLoading from "../../components/queryLoading";
 
 const UserSpace: React.FunctionComponent = memo(() => {
   // Auth
   const { idToken, currentUser } = useAuth();
-  const userQuery = useGetUserByFirebaseIdQuery({ endpoint, fetchParams: { headers: headers(idToken) } });
+  const userQuery = useGetUserByFirebaseIdQuery({
+    endpoint,
+    fetchParams: { headers: headers(idToken) },
+  });
 
   // Filter
-  const [search, setSearch] = useState<string>('');
+  const [search, setSearch] = useState<string>("");
 
   if (userQuery.isLoading) {
     return <QueryLoading size={80} speed={1} color="#363E4A" />;
@@ -34,7 +43,9 @@ const UserSpace: React.FunctionComponent = memo(() => {
         <Heading as="h5" size="md" fontWeight={300}>
           {userQuery.isSuccess && userQuery.data.user.userName}
         </Heading>
-        {!currentUser?.emailVerified && <Text fontSize="sm">Please do not forget to verify your email!</Text>}
+        {!currentUser?.emailVerified && (
+          <Text fontSize="sm">Please do not forget to verify your email!</Text>
+        )}
       </Box>
 
       <Accordion allowToggle defaultIndex={0}>
@@ -42,9 +53,9 @@ const UserSpace: React.FunctionComponent = memo(() => {
         <AccordionItem>
           <h2>
             <AccordionButton
-              _expanded={{ bg: '#3b4453', fontWeight: 'bolder' }}
-              _hover={{ bg: '#272c36' }}
-              _focus={{ boxShadow: 'none' }}
+              _expanded={{ bg: "#3b4453", fontWeight: "bolder" }}
+              _hover={{ bg: "#272c36" }}
+              _focus={{ boxShadow: "none" }}
             >
               <Box pl="5" pr="5" flex="1" textAlign="center">
                 Your templates
@@ -56,24 +67,14 @@ const UserSpace: React.FunctionComponent = memo(() => {
               <Input
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
-                placeholder="Search by name"
+                placeholder="Search"
               />
-              <Icon
-                cursor="pointer"
-                viewBox="0 0 24 24"
-                fill="white"
-                transition="0.3s"
-                opacity={0.5}
-                _hover={{ opacity: 0.8 }}
-              >
-                <path
-                  xmlns="http://www.w3.org/2000/svg"
-                  d="M7,6H6V3A1,1,0,0,0,4,3V6H3A1,1,0,0,0,3,8H7A1,1,0,0,0,7,6ZM5,10a1,1,0,0,0-1,1V21a1,1,0,0,0,2,0V11A1,1,0,0,0,5,10Zm7,8a1,1,0,0,0-1,1v2a1,1,0,0,0,2,0V19A1,1,0,0,0,12,18Zm9-8H20V3a1,1,0,0,0-2,0v7H17a1,1,0,0,0,0,2h4a1,1,0,0,0,0-2Zm-2,4a1,1,0,0,0-1,1v6a1,1,0,0,0,2,0V15A1,1,0,0,0,19,14Zm-5,0H13V3a1,1,0,0,0-2,0V14H10a1,1,0,0,0,0,2h4a1,1,0,0,0,0-2Z"
-                />
-              </Icon>
             </HStack>
             {userQuery.isSuccess && userQuery.data && (
-              <Templates search={search} designs={userQuery.data.user.designs as Design[]} />
+              <Templates
+                search={search}
+                designs={userQuery.data.user.designs as Design[]}
+              />
             )}
           </AccordionPanel>
         </AccordionItem>
@@ -82,9 +83,9 @@ const UserSpace: React.FunctionComponent = memo(() => {
         <AccordionItem>
           <h2>
             <AccordionButton
-              _expanded={{ bg: '#3b4453', fontWeight: 'bolder' }}
-              _hover={{ bg: '#272c36' }}
-              _focus={{ boxShadow: 'none' }}
+              _expanded={{ bg: "#3b4453", fontWeight: "bolder" }}
+              _hover={{ bg: "#272c36" }}
+              _focus={{ boxShadow: "none" }}
             >
               <Box flex="1" textAlign="center">
                 Explore Templates

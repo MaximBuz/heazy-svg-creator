652ed22b1f1a869c5d56ce7325fbf515532bd4ae
Maxim Buz <mbuz.maxim@gmail.com>
2023-07-11 16:39:58 +0200

    fix: type errors and console logs

--- server/firebase.ts
+++ server/firebase.ts
@@ -1,13 +1,11 @@
 import { initializeApp } from 'firebase-admin/app';
-const firebaseCert = require('firebase-admin');
-const dotenv = require('dotenv');
+import firebaseCert from 'firebase-admin';
+import dotenv from 'dotenv';
 dotenv.config();
-// @ts-ignore
-console.log(JSON.parse(process.env.GOOGLE_CREDS));
+
 const admin = initializeApp({
   credential: firebaseCert.credential.cert(
-    // @ts-ignore
-    JSON.parse(process.env.GOOGLE_CREDS)
+    JSON.parse(process.env.GOOGLE_CREDS as string)
   ),
 });
 export default admin;

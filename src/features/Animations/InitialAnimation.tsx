ba50f8e0e915f378d338a81091cf2e0ad48b88ea
Maxim Buz <mbuz.maxim@gmail.com>
2023-07-11 17:03:03 +0200

    fix: unreponsive app while initial animation

--- src/features/Animations/InitialAnimation.tsx
+++ src/features/Animations/InitialAnimation.tsx
@@ -5,11 +5,13 @@ import LogoAnimation from './LogoAnimation';
 function InitialAnimation() {
   const [isRunning, setIsRunning] = useState<boolean>(true);
   const [opacity, setOpacity] = useState<number>(1);
+  const [mouseActive, setMouseActive] = useState<boolean>(false);
 
   useEffect(() => {
     (async () => {
       await setTimeout(() => {
         setOpacity(0);
+        setMouseActive(true);
       }, 2000);
 
       await setTimeout(() => {
@@ -38,6 +40,7 @@ function InitialAnimation() {
           transform: 'translate(-50%, -50%)',
           opacity: opacity,
           transition: 'all 1s ease',
+          pointerEvents: mouseActive ? 'none' : 'auto',
         }}
       >
         <LogoAnimation />

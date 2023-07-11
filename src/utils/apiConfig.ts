30687212a53b46dc3947ae6d6125866ec88e7d53
Maxim Buz <mbuz.maxim@gmail.com>
2023-07-11 23:11:44 +0200

    fix: correctly apply environment variables

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,5 +1,6 @@
 export const endpoint =
-  process.env.API_URL || 'https://heazy-backend.herokuapp.com/graphql';
+  process.env.REACT_APP_API_URL ||
+  'https://heazy-backend.herokuapp.com/graphql';
 
 export const headers = (idToken: string) => {
   if (idToken)

9409e5bf71be4d72b910ca1d766c5cb762d35dcb
Maxim Buz <mbuz.maxim@gmail.com>
2023-07-12 11:06:46 +0200

    feat: set up i18n next

--- heazy-landing-page/next.config.js
+++ heazy-landing-page/next.config.js
@@ -1,12 +1,13 @@
+const { i18n } = require('./next-i18next.config');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true,
-    domains: [
-      'api.producthunt.com',
-    ],
+    domains: ['api.producthunt.com'],
   },
-}
+  i18n,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

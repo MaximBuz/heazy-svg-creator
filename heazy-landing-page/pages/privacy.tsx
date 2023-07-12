df3aa266f52a8f71fa7862d2a8091e7e2e5e33cf
Maxim Buz <mbuz.maxim@gmail.com>
2023-07-12 11:21:58 +0200

    fix: translations on pricacy policy page

--- heazy-landing-page/pages/privacy.tsx
+++ heazy-landing-page/pages/privacy.tsx
@@ -685,4 +685,14 @@ Germany`}
   );
 };
 
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+export async function getStaticProps({ locale }: { locale: any }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'navbar'])),
+    },
+  };
+}
+
 export default Privacy;

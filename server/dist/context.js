af9092214249036819ad066d97005200704cc04c
Maxim Buz <mbuz.maxim@gmail.com>
2023-07-13 20:48:03 +0200

    chore: remove dist folder

--- server/dist/context.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.context = void 0;
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-exports.context = { prisma };

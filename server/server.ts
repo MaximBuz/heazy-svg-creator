6dafaca7c32b50b6d6e00fb9645b717aa7572f91
Maxim Buz <mbuz.maxim@gmail.com>
2023-07-14 16:31:12 +0200

    refactor: promisify jwt authentication

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { graphqlHTTP } from 'express-graphql';
 import { schema } from './src';
 import { context } from './context';
-import { DecodedIdToken, getAuth } from 'firebase-admin/auth';
+import { getAuth } from 'firebase-admin/auth';
 import admin from './firebase';
 
 const PORT = process.env.PORT || 4000;
@@ -13,22 +13,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use((req, _res, next) => {
+app.use(async (req, _res, next) => {
   const authHeader = req.headers.authorization;
 
   if (authHeader) {
     const idToken = authHeader.split(' ')[1];
-    getAuth(admin)
-      .verifyIdToken(idToken)
-      .then((decodedToken: DecodedIdToken) => {
-        const uid = decodedToken.uid;
-        context.uid = uid; // pass uid of user to graphql resolvers
-        next();
-      })
-      .catch((error) => {
-        console.error(error);
-        next();
-      });
+    try {
+      const decodedToken = await getAuth(admin).verifyIdToken(idToken);
+      const uid = decodedToken.uid;
+      context.uid = uid; // pass uid of user to GraphQL resolvers
+      next();
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
   } else {
     next();
   }
